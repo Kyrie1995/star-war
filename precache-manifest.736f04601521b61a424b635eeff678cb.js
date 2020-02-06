05d9816a9c580f4855a1a1241832a0fde17f1fa2
Kyrie1995 <lzhsummer@gmail.com>
2020-02-06 19:27:05 +1100

    Updates

---
+++ precache-manifest.736f04601521b61a424b635eeff678cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8344b6b421c37fe4e20d975304771d1",
+    "url": "/star-war/index.html"
+  },
+  {
+    "revision": "d9f7eba29c5bbe7383af",
+    "url": "/star-war/static/js/2.01656fa6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/star-war/static/js/2.01656fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1eadf1b5130717d543",
+    "url": "/star-war/static/js/main.298d4684.chunk.js"
+  },
+  {
+    "revision": "a0dca9108aad5389c87c",
+    "url": "/star-war/static/js/runtime-main.3b1e7e3a.js"
+  }
+]);
